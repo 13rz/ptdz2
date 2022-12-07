67926053be339ad655d1632982200850e0161dad
13rz <zhurenkoroma13@gmail.com>
2022-12-07 08:51:50 +0200

    add homework

---
+++ dz2.py
@@ -0,0 +1,19 @@
+print("\n\timport antigravity"
+      "\n\timport __hello__"
+      "\n\tfrom __future__ import braces")
+
+print("***     **    *   *    ** \n"
+      "*  *   *  *   ** **   *  *\n"
+      "*  *   *  *   ** **   *  *\n"
+      "***    *  *   * * *   ****\n"
+      "* *    *  *   *   *   *  *\n"
+      "*  *    **    *   *   *  *")
+
+print()
+print("\\a\t\tBell (alert)")
+print("\\b\t\tBackspace")
+print("\\n\t\tNew line")
+print("\\t\t\tHorizontal tab")
+print("\\\t\tBackslash \ ")
+print('\\"\t\tDouble quotation mark "')
+print("\\'\t\tSingle quotation mark '")
